de5965618a8bbf9dd2c729dcc25db02d352eb673
William Chu <will@Williams-MacBook-Pro.local>
2022-05-12 17:55:03 -0700

    updated structure and added bin-to-dec

---
+++ Codesmith/bin-to-dec.js
@@ -0,0 +1,24 @@
+/* 
+
+Given a string that represents a Binary Number, write a function that converts this string into a decimal number. DO NOT use the native parseInt() method. 
+
+For example: 
+
+binToDec('0')   -> 0
+binToDec('11')  -> 3
+binToDec('100') -> 4
+binToDec('101') -> 5
+binToDec('0101') -> 5
+
+For more information on how to read binary, check out this article: 
+https://m.wikihow.com/Read-Binary
+
+*/
+
+function binToDec(binString){
+
+}
+
+
+module.exports = {binToDec};
+
