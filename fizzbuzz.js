34316121d87ad2722b91f33ea5549d32b6757056
William Chu <will@Williams-MacBook-Pro.local>
2022-05-12 17:33:32 -0700

    adding fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,40 @@
+/*
+
+Write a function that returns an array containing the numbers 1 to NUM.
+
+Put "fizz" in place of numbers divisble by 3 but not by 5,
+"buzz" in place of numbers divisble by 5 but not by 3,
+and "fizzbuzz" in place of numbers divisble by both 3 and 5.
+
+fizzbuzz(16);
+-> [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz',
+11, 'fizz', 13, 14, 'fizzbuzz', 16]
+
+*/
+
+const fizzbuzz = num => {  
+
+};
+
+/*
+
+Extension: 
+Write a function that returns an array containing the numbers 1 to NUM.
+
+Put "fizz" in place of numbers divisble by 3 but not by 5 or 7,
+"buzz" in place of numbers divisble by 5 but not by 3 or 7,
+"bazz" in place of numbers divisble by 7 but not by 3 or 5,
+"fizzbuzz" in place of numbers divisble by 3 and 5 but not 7,
+"fizzbazz" in place of numbers divisble by 3 and 7 but not 5,
+"buzzbazz" in place of numbers divisble by 5 and 7 but not 3,
+and "fizzbuzzbazz" in place of numbers divisble by 3, 5, and 7.
+
+fizzbuzzbazz(22);
+-> [1, 2, 'fizz', 4, 'buzz', 'fizz', 'bazz', 8, 'fizz', 'buzz',
+11, 'fizz', 13, 'bazz', 'fizzbuzz', 16, 17, 'fizz', 19, 'buzz', 'fizzbazz', 22]
+
+*/
+
+const fizzbuzzbazz = num => {
+
+};
